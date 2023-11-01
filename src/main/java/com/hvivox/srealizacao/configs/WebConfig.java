afee2b10e88f95428c44be36021cacdcac8fbe87
hvivox <hvivox@hotmail.com>
2023-11-01 08:51:11 -0400

    - Continuação Cors criação de class para habilitar a url do frontend chamar a aplicação backend

--- src/main/java/com/hvivox/srealizacao/configs/WebConfig.java
+++ src/main/java/com/hvivox/srealizacao/configs/WebConfig.java
@@ -1,2 +1,24 @@
-package com.hvivox.srealizacao.configs;public class WebConfig {
+package com.hvivox.srealizacao.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite mapear todos os endpoints da API
+                        .allowedOrigins("http://localhost:5173") // URL da sua aplicação React
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
