475f5ab3a9a63a5baca64179429d36e0b67c70dd
hvivox <hvivox@hotmail.com>
2024-09-23 15:58:08 -0400

    [SREALIZACAOV1-04] Configuração de log com log back - remoção do log4j2 para não conflitar com o logback - inclusão de libs para o logback - AuthController: criação de log em conjunto como lombok para servir de exemplo - AuthController: configuração para mostrar infomações de logs essenciais e organização - logback-spring: configuração do formato do log

--- src/main/java/com/hvivox/srealizacao/controller/AuthController.java
+++ src/main/java/com/hvivox/srealizacao/controller/AuthController.java
@@ -2,16 +2,19 @@ package com.hvivox.srealizacao.controller;
 
 import com.hvivox.srealizacao.model.UserKeycloack;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@Valid @RequestBody UserKeycloack userKeycloack) {
+        log.info("Login: {}", userKeycloack.toString());
         return ResponseEntity.ok(userKeycloack);
     }
 
